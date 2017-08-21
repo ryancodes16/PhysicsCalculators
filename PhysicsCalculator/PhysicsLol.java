f481c2c19e86a1d6ef7ed27743a1cd37a93ebaae
Ryan <ryancodes16@gmail.com>
2017-08-21 18:08:12 -0500

    Setting up the program, nothing big yet.
    Will finish it up by Saturday 8/26/2017

---
+++ PhysicsCalculator/PhysicsLol.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/**
+ * Calculates basic physics stuff
+ * 
+ * @Ryan Regier
+ * @1.0 
+ * @8/21/17
+ */
+public class PhysicsLol
+{
+   public static void main(String args[])
+   {
+       Scanner scan = new Scanner(System.in);
+       int ans = 0;
+       System.out.println("Enter the number for whatever equation you want.[1 = speed, 2 = displacement, 3 = average velocity, 4 = average speed, 5 = slope, 6 = average acceleration]");
+       ans = scan.nextInt();
+    }
+}
