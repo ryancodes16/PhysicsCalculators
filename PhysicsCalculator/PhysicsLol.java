62218169de3ee958ff6b2c4655dd9a402e8f607d
Ryan <ryancodes16@gmail.com>
2017-08-21 20:18:58 -0500

    Added speed & displacement formulas to calculator

--- PhysicsCalculator/PhysicsLol.java
+++ PhysicsCalculator/PhysicsLol.java
@@ -10,9 +10,37 @@ public class PhysicsLol
 {
    public static void main(String args[])
    {
+       double d, t, Di, Df;
        Scanner scan = new Scanner(System.in);
        int ans = 0;
        System.out.println("Enter the number for whatever equation you want.[1 = speed, 2 = displacement, 3 = average velocity, 4 = average speed, 5 = slope, 6 = average acceleration]");
        ans = scan.nextInt();
+       System.out.println("Enter your distance in meters and your time in seconds");
+       if(ans == 1)
+       {
+           System.out.println("Enter distance");
+           d = scan.nextDouble();
+           System.out.println("Enter time");
+           t = scan.nextDouble();
+           System.out.println(speed(d, t) + " m/s");
+        }
+       else if(ans == 2)
+       {
+           System.out.println("Enter final distance");
+           Df = scan.nextDouble();
+           System.out.println("Enter initial distance");
+           Di = scan.nextDouble();
+           System.out.println("Enter time");
+           t = scan.nextDouble();
+           System.out.println(displacement(Df, Di, t) + " m/s");           
+        }
+    }
+   public static double speed(double d, double t)
+   {
+       return(d/t);      
+    }
+    public static double displacement(double Df, double Di, double t)
+   {
+       return((Df-Di)/t);      
     }
 }
